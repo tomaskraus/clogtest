ba90fe77fc86509a89878a50abf6cf197a4692b1
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-07-21 18:46:39 +0200

    docs(examples): error throwing example

--- examples/ts-example.ts
+++ examples/ts-example.ts
@@ -1,17 +1,21 @@
-{
-  const inverse = (a: number) => {
-    if (a === 0) {
-      throw new Error("Cannot inverse a zero!");
-    }
-    return 1 / a;
-  };
+const inverse = (a: number) => {
+  if (a === 0) {
+    throw new Error("Cannot inverse a zero!");
+  }
+  return 1 / a;
+};
 
-  console.log(inverse(1));
-  //=> 1
+console.log(inverse(1));
+//=> 1
 
-  /* console.log(inverse(4));
-//=> 0.25 */
+console.log(inverse(3));
+//=> 0.333 ...
 
-  console.log(inverse(3));
-  //=> 0.333 ...
+try {
+  console.log(inverse(0));
+} catch (err) {
+  if (err instanceof Error) {
+    console.log(err.message);
+    //=> ... zero ...
+  }
 }
--- test/inputs/ts-input.ts
+++ test/inputs/ts-input.ts
@@ -1,15 +1,17 @@
-const inverse = (a: number) => {
-  if (a === 0) {
-    throw new Error("Cannot inverse a zero!");
-  }
-  return 1 / a;
-};
+{
+  const inverse = (a: number) => {
+    if (a === 0) {
+      throw new Error("Cannot inverse a zero!");
+    }
+    return 1 / a;
+  };
 
-console.log(inverse(1));
-//=> 1
+  console.log(inverse(1));
+  //=> 1
 
-// console.log(inverse(4));
-// //=> 0.25
+  // console.log(inverse(4));
+  // //=> 0.25
 
-console.log(inverse(3));
-//=> 0.33 ...
+  console.log(inverse(3));
+  //=> 0.33 ...
+}
