1ce636ee480c2edf6a62ee51389ca08eaf809b48
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-07-25 03:28:09 +0200

    refactor: use engine in main.js

--- src/main.js
+++ src/main.js
@@ -1,92 +1,46 @@
-const { getTestInputAndSource, runTests } = require("./core.js");
+const engine = require("./engine.js");
 const { printResults, out } = require("./report.js");
 const { appLog } = require("./utils.js");
 const log = appLog.extend("main");
-const SSP = require("simple-string-pattern").default;
-const fs = require("fs/promises");
 
 const appName = require("../package.json").name;
-
-const MAX_WRITTEN_PATTERN_LENGTH = 20;
-
-const TEST_MARK = "//=>";
 
 const printHeader = (action, fileName) => {
   out(`${appName} ${action}: ${fileName}`);
 };
 
-/**
- *
- * @param {string} fileName
- * @param {string | null} tsFileName
- * @returns
- */
-const doTests = async (fileName, tsFileName = null) => {
-  const [testInputs] = await getTestInputAndSource(
-    TEST_MARK,
-    fileName,
-    tsFileName
-  );
-  return runTests(testInputs);
-};
-
 // ------------------
 
 const doTestsAndPrintResults = async (fileName, tsFileName = null) => {
+  log(`doTestsAndPrintResults: START ---------`);
   printHeader("test", tsFileName || fileName);
-  const [testInputs, source] = await getTestInputAndSource(
-    TEST_MARK,
+  const [allResults, fails, source] = await engine.doTests(
     fileName,
     tsFileName
   );
-  const [allResults, fails] = runTests(testInputs);
   printResults(allResults, fails, tsFileName || fileName, source);
+  log(`doTestsAndPrintResults: END - - - - -`);
   return fails.length === 0 ? 0 : 1;
 };
 
 const writeAssertions = async (fileName, tsFileName = null) => {
-  printHeader("write-assertions", fileName);
-  const [testInputs, inputs] = await getTestInputAndSource(
-    TEST_MARK,
-    fileName,
-    tsFileName
-  );
-  let testInputIndex = 0;
-  let assertionsFilledCount = 0;
+  log(`writeAssertions: START ----------`);
 
-  const content = inputs
-    .map((line, index) => {
-      let s = line;
-      let lineIndex = index + 1;
-      if (
-        testInputIndex < testInputs.length &&
-        lineIndex === testInputs[testInputIndex].lineNumber
-      ) {
-        if (testInputs[testInputIndex].expected === "") {
-          s =
-            testInputs[testInputIndex].linePadding +
-            TEST_MARK +
-            " " +
-            SSP.parse(testInputs[testInputIndex].received)
-              .limitPatternLen(MAX_WRITTEN_PATTERN_LENGTH)
-              .value();
-          assertionsFilledCount++;
-          log(`write assertion [${lineIndex}] [${s}]`);
-          out(`\t${fileName}:${lineIndex}`);
-        }
-        testInputIndex++;
-      }
-      return s;
-    })
-    .join("\n");
-  if (assertionsFilledCount > 0) {
-    await fs.writeFile(tsFileName || fileName, content);
-  }
+  const printLineHandler = (fileName, lineNumber, line) =>
+    out(`\t${fileName}:${lineNumber}\t${line.trim()}`);
+
+  const sourceFileName = tsFileName || fileName;
+  printHeader("write-assertions: ", sourceFileName);
+  const assertionsFilledCount = await engine.writeAssertions(
+    fileName,
+    tsFileName,
+    printLineHandler
+  );
   out(`${assertionsFilledCount} assertion comment(s) filled`);
+  log(`writeAssertions: END - - - - -`);
 };
 
 module.exports = {
   doTestsAndPrintResults,
-  doTests,
   writeAssertions,
 };
