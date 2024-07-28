6c52e333b9457827a4f1bede7c929ab286eadee7
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-07-28 13:57:05 +0200

    tests: add test of ts source fill-assertion

--- test/engine.write-assertions.test.js
+++ test/engine.write-assertions.test.js
@@ -25,4 +25,19 @@ describe("write-assertions:", () => {
     expect(assertions[2]).toEqual("2");
     expect(callbackCallCount).toEqual(2);
   });
+
+  test("TypeScript source: fills an empty assertion correctly.", async () => {
+    const [content, filledCount] = await fillAssertions(
+      "dist/test/inputs/empty-assertion2.js",
+      "test/inputs/empty-assertion2.ts",
+      onTestMark
+    );
+    const assertions = content
+      .filter((line) => line.trim().startsWith(testMark))
+      .map((line) => line.slice(testMark.length).trim());
+    expect(filledCount).toEqual(2);
+    expect(assertions[0]).toEqual("[ 1, 2, 3, 4, 5 ]");
+    expect(assertions[2]).toEqual("2");
+    expect(callbackCallCount).toEqual(2);
+  });
 });
---
+++ test/inputs/empty-assertion2.ts
@@ -0,0 +1,7 @@
+const nums: number[] = [1, 2, 3, 4, 5];
+console.log(nums);
+//=>
+console.log("hello");
+//=> "hello"
+console.log(1 + 1);
+//=>
