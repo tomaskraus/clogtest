089f7dd76edd66069a8ced45023d40d458b78762
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-09-04 10:58:20 +0200

    fix(tests): old doTests method signature

--- test/assertion-mark.test.js
+++ test/assertion-mark.test.js
@@ -3,7 +3,7 @@ const engineProvider = require("../src/engine");
 const { doTests, getStats } = engineProvider();
 
 test("Cannot be fooled with printed assertion mark.", async () => {
-  const [results] = await doTests("./test/inputs/print-assertion-mark.js");
+  const results = await doTests("./test/inputs/print-assertion-mark.js");
   const { totalCount, failedCount } = getStats(results);
   expect(totalCount).toEqual(5);
   expect(failedCount).toEqual(0);
